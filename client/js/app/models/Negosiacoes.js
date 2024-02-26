914d6337f37590bb152fb477b38907c5e2036e37
jotafagundezz06 <jotafagundezz@gmail.com>
2024-02-26 16:53:20 -0300

    metodo get aplicado e tudo funcionando no terminal

--- client/js/app/models/Negosiacoes.js
+++ client/js/app/models/Negosiacoes.js
@@ -1,17 +1,30 @@
 class negosiacoes {
     // Define uma classe chamada negosiacoes
-
-    constructor() {
+    constructor(data, quantidade, valor) {
         // Define o método especial constructor(), que é chamado automaticamente quando um objeto desta classe é instanciado
-
-        this.data = new Date();
+        this._data = data;
         // Cria uma propriedade chamada data e atribui a ela um novo objeto Date(), que representa a data e hora atuais
-
-        this.quantidade = 1;
+        this._quantidade = quantidade ;
         // Cria uma propriedade chamada quantidade e atribui o valor inicial de 1 a ela
-
-        this.valor = 0,0;
+        this._valor = valor ;
         // Cria uma propriedade chamada valor e atribui o valor inicial de 0 a ela (presumo que o valor correto seja 0.0 em vez de 0,0)
         // NOTA: No entanto, há um erro de sintaxe aqui. A vírgula deve ser substituída por um ponto para representar corretamente um número de ponto flutuante.
     }
+
+    get volume(){
+        return this._quantidade * this._valor;
+    }
+
+    get data() {
+        return this._data;
+    }
+    
+    get quantidade() {
+        return this._quantidade;
+    }
+    
+    get valor() {
+        return this._valor;
+    }
 }
+
