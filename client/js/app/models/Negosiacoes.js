1d994af2b4c9b333f3df8aa70b06b4133879ac80
jotafagundezz06 <jotafagundezz@gmail.com>
2024-02-28 16:01:19 -0300

    fim do codigo controler e fim metodo getter funcionando

--- client/js/app/models/Negosiacoes.js
+++ client/js/app/models/Negosiacoes.js
@@ -1,30 +1,39 @@
-class negosiacoes {
-    // Define uma classe chamada negosiacoes
+class Negociacoes {
+    // Define uma classe chamada Negociacoes
     constructor(data, quantidade, valor) {
         // Define o método especial constructor(), que é chamado automaticamente quando um objeto desta classe é instanciado
-        this._data = data;
-        // Cria uma propriedade chamada data e atribui a ela um novo objeto Date(), que representa a data e hora atuais
-        this._quantidade = quantidade ;
-        // Cria uma propriedade chamada quantidade e atribui o valor inicial de 1 a ela
-        this._valor = valor ;
-        // Cria uma propriedade chamada valor e atribui o valor inicial de 0 a ela (presumo que o valor correto seja 0.0 em vez de 0,0)
-        // NOTA: No entanto, há um erro de sintaxe aqui. A vírgula deve ser substituída por um ponto para representar corretamente um número de ponto flutuante.
+        this._data = new Date(data.getTime());
+        // Cria uma propriedade chamada _data e atribui a ela um novo objeto Date() baseado na data fornecida
+        this._quantidade = quantidade;
+        // Cria uma propriedade chamada _quantidade e atribui o valor passado como argumento
+        this._valor = valor;
+        // Cria uma propriedade chamada _valor e atribui o valor passado como argumento
+        // NOTA: Há um erro de sintaxe aqui. A vírgula deve ser substituída por um ponto para representar corretamente um número de ponto flutuante.
+        Object.freeze(this);
+        // Congela o objeto, tornando-o imutável (não permite alterações em suas propriedades)
     }
 
     get volume(){
+        // Define um método getter chamado volume
         return this._quantidade * this._valor;
+        // Retorna o volume da negociação (quantidade * valor)
     }
 
     get data() {
-        return this._data;
+        // Define um método getter chamado data
+        return new Date(this._data.getTime());
+        // Retorna uma nova instância de Date() com o valor da data armazenada em _data, garantindo imutabilidade
     }
     
     get quantidade() {
+        // Define um método getter chamado quantidade
         return this._quantidade;
+        // Retorna o valor armazenado em _quantidade
     }
     
     get valor() {
+        // Define um método getter chamado valor
         return this._valor;
+        // Retorna o valor armazenado em _valor
     }
 }
-
